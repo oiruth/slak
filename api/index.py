812cad04c430cd0c80ecac451944976065551b19
oiruth <ruths9631@gmail.com>
2025-03-31 19:35:42 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -50,7 +50,8 @@ def send_code_to_telegram():
 # Rota para servir o HTML
 @app.route('/')
 def serve_html():
-    return send_from_directory(os.getcwd(), 'index.html')
+    return send_from_directory(os.path.join(app.root_path, 'index.html'))
+
 
 # Rota para gerar a chave
 @app.route('/generate_key', methods=['POST'])
